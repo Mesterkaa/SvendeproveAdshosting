d0364fc704b3727774808da4d1342ba337ed51f4
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2022-05-31 12:24:17 +0200

    #13 Base with errors

---
+++ server/config/secrets.ts
@@ -0,0 +1,24 @@
+import {logger} from "./logger";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const MONGODB_URI = process.env["MONGODB_URI"];
+
+if (!MONGODB_URI) {
+    logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
+    process.exit(1);
+}
+
+export const JWT_SECRET = process.env["JWT_SECRET"];
+
+if (!JWT_SECRET) {
+    logger.error("No JWT secret string. Set JWT_SECRET environment variable.");
+    process.exit(1);
+}
+
+export const PORT = Number(process.env["PORT"]);
+
+if (!(PORT > 0)) {
+  logger.error("No Port number set. Set PORT environment variable.");
+  process.exit(1);
+}
