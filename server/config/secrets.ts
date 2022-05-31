0edaf0aee2e699a3fd796478608e30805ffc05b4
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2022-05-31 13:46:29 +0200

    #13 Base angular

--- server/config/secrets.ts
+++ server/config/secrets.ts
@@ -4,17 +4,12 @@ dotenv.config();
 
 export const MONGODB_URI = process.env["MONGODB_URI"];
 
+/*
 if (!MONGODB_URI) {
     logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
     process.exit(1);
 }
-
-export const JWT_SECRET = process.env["JWT_SECRET"];
-
-if (!JWT_SECRET) {
-    logger.error("No JWT secret string. Set JWT_SECRET environment variable.");
-    process.exit(1);
-}
+*/
 
 export const PORT = Number(process.env["PORT"]);
 
