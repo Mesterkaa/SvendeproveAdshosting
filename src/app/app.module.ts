4b0a410605350757c7a3740aed39cbdc0dcd7326
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2022-06-09 10:49:23 +0200

    #22 Implemented Azure on frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http"; // Import
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +19,12 @@ import { AccountComponent } from './pages/customer/account/account.component';
 import { BuyComponent } from './pages/customer/buy/buy.component';
 import { RepoComponent } from './pages/customer/repo/repo.component';
 import { LoginComponent } from './pages/login/login.component';
+
+import { MsalModule, MsalGuard, MsalInterceptor } from '@azure/msal-angular'; // Import MsalInterceptor
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+
 
 @NgModule({
   declarations: [
@@ -30,9 +42,42 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    HttpClientModule,
+    MsalModule.forRoot( new PublicClientApplication({
+      auth: {
+        clientId: '47ad7db1-f067-4ba4-864c-edd1a5e73fd7',
+        authority: 'https://login.microsoftonline.com/806ab42c-cd67-45a5-80ca-af7e90cfa6e7',
+        redirectUri: 'http://localhost:4200'
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE,
+      }
+    }), {
+      interactionType: InteractionType.Popup, // MSAL Guard Configuration
+      authRequest: {
+        scopes: ['user.read']
+      }
+  }, {
+    interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
+    protectedResourceMap: new Map([
+        ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+    ])
+  })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/customer/account/account.component.ts
+++ src/app/pages/customer/account/account.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0/me/';
+
+type ProfileType = {
+  givenName?: string,
+  surname?: string,
+  userPrincipalName?: string,
+  id?: string
+};
 
 @Component({
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
+  profile!: ProfileType;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getProfile();
   }
 
+  getProfile() {
+    this.http.get(GRAPH_ENDPOINT)
+      .subscribe(profile => {
+        this.profile = profile;
+      });
+  }
 }
