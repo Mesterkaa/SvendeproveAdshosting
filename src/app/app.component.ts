4da0a820cc847a642fee2fb21fd0780f21b0487c
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2022-06-09 12:56:51 +0200

    #23 Added userService to filter Guard by id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MsalService, MsalBroadcastService, MSAL_GUARD_CONFIG, MsalGuardConfigur
 import { InteractionStatus, PopupRequest } from '@azure/msal-browser';
 import { Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,10 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'Ads-Web-Applikation';
   isIframe = false;
   loginDisplay = false;
+  name = "";
   private readonly _destroying$ = new Subject<void>();
 
-  constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration, private broadcastService: MsalBroadcastService, private authService: MsalService) { }
+  constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration, private broadcastService: MsalBroadcastService, private authService: MsalService, private userService: UserService) { }
 
   ngOnInit() {
     this.isIframe = window !== window.parent && !window.opener;
@@ -28,33 +30,31 @@ export class AppComponent implements OnInit, OnDestroy {
     .subscribe(() => {
       this.setLoginDisplay();
     })
+
+    this.userService.user
+    .pipe(takeUntil(this._destroying$))
+    .subscribe((e) => {
+      this.name = e[0] ? e[0] : "";
+    })
   }
 
   login() {
-    if (this.msalGuardConfig.authRequest){
-      this.authService.loginPopup({...this.msalGuardConfig.authRequest} as PopupRequest)
+    this.authService.loginPopup(this.msalGuardConfig.authRequest ? {...this.msalGuardConfig.authRequest} as PopupRequest : undefined)
         .subscribe({
           next: (result) => {
             console.log(result);
+            this.userService.updateLogin(result.account?.name, result.account?.localAccountId);
             this.setLoginDisplay();
           },
           error: (error) => console.log(error)
         });
-    } else {
-      this.authService.loginPopup()
-        .subscribe({
-          next: (result) => {
-            console.log(result);
-            this.setLoginDisplay();
-          },
-          error: (error) => console.log(error)
-        });
-    }
   }
 
   logout() { // Add log out function here
     this.authService.logoutPopup({
       mainWindowRedirectUri: "/"
+    }).toPromise().then( () => {
+      this.userService.updateLogin(undefined,undefined);
     });
   }
 
--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -2,21 +2,28 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MsalService } from '@azure/msal-angular';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
 
-  constructor(private _msalService: MsalService) { }
+  private id: string;
+  constructor(private msalService: MsalService, private userService: UserService) {
+    this.userService.user.subscribe((e) => {
+      this.id = e[1] ? e[1] : "";
+    })
+   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const userRoles = this._msalService.instance.getAllAccounts()[0]?.idTokenClaims?.roles; //This needs to changed to where the Account is pulled based on Local Id
+    const userRoles = this.msalService.instance.getAccountByLocalId(this.id)?.idTokenClaims?.roles; //This needs to changed to where the Account is pulled based on Local Id
     if (userRoles == undefined) return false;
     const allowedRoles = next.data["roles"];
     const matchingRoles = userRoles.filter(x => allowedRoles.includes(x));
     return matchingRoles.length > 0;
+
   }
 }
