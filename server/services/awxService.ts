83ea12916742c65f73f9d1037b24b077e9748aad
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2022-06-20 20:54:15 +0200

    #38 API changes

--- server/services/awxService.ts
+++ server/services/awxService.ts
@@ -1,4 +1,4 @@
-import { awx_token } from '../config/config.json'
+import { awx_token, api_urls } from '../config/config.json'
 import { ILicense, License } from '../models/license';
 import { ICompany, Company } from '../models/company';
 import { IProduct, Product } from '../models/product';
@@ -20,7 +20,7 @@ export class AwxService {
    */
   public async launchJob(stage: string, product: IProduct, company: ICompany): Promise<string> {
     return await this.axiosInstance
-    .post(`https://awx.adshosting.lan/api/v2/workflow_job_templates/17/launch/`,
+    .post(`${api_urls.awx}/workflow_job_templates/17/launch/`,
       { 'extra_vars': {
       'company': company.Name.toLocaleLowerCase(),
       'stage': stage,
@@ -44,7 +44,7 @@ export class AwxService {
    */
   public async getJobStatus(jobId: string): Promise<string> {
     return await this.axiosInstance
-    .get(`https://awx.adshosting.lan/api/v2/workflow_jobs/${jobId}/`, {headers: { 'Authorization': `Bearer ${awx_token}`}})
+    .get(`${api_urls.awx}/workflow_jobs/${jobId}/`, {headers: { 'Authorization': `Bearer ${awx_token}`}})
     .then(res => {
       return res.data["status"];
     })
@@ -57,15 +57,35 @@ export class AwxService {
    * Delete job
    * @param jobId Id of the wanted job
    */
-   public async deleteJob(jobId: string): Promise<void> {
-    /*return await this.axiosInstance
-    .get(`https://awx.adshosting.lan/api/v2/workflow_jobs/${jobId}/`, {headers: { 'Authorization': `Bearer ${awx_token}`}})
+   public async deleteJob(jobId: string): Promise<any> {
+    const cluster = "";
+    const gitlabproject = "";
+    return await this.axiosInstance
+    .post(`${api_urls.awx}/workflow_job_templates/17/launch/`,
+      { 'extra_vars': {
+        'cluster': cluster,
+        'gitlabproject': gitlabproject
+      }},
+      {headers: { 'Authorization': `Bearer ${awx_token}`}})
     .then(res => {
-      return res.data["status"];
+      return (res.data.id as string);
     })
-    .catch(error => {
+    .catch((error: any) => {
       throw error;
-    })*/
+    })
+  }
+
+  public async clusterId(jobId: string): Promise<string> {
+    const intJobId = Number(jobId);
+    if (intJobId == NaN) throw new Error("jobId is not a number");
+    return await this.axiosInstance
+      .get(`${api_urls.awx}/jobs/${intJobId + 1}`, {headers: { 'Authorization': `Bearer ${awx_token}`}})
+      .then(res => {
+        return res.data["clusterfacts"]["id"];
+      })
+      .catch((error: any) => {
+        throw error;
+      })
   }
 
 }
